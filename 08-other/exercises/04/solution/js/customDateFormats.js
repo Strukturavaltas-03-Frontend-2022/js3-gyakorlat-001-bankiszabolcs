0b28dc7db0e17eb9283d08c0fd98fb9bc7f8cd1b
szabi1112 <banki.szabolcs@gmail.com>
2022-08-31 00:59:43 +0200

    Kész

--- 08-other/exercises/04/solution/js/customDateFormats.js
+++ 08-other/exercises/04/solution/js/customDateFormats.js
@@ -1,1 +1,12 @@
+const customDateFormats = function (date = new Date()) {
+  return {
+    long: new Intl.DateTimeFormat('hu-HU', {
+      year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric',
+    }).format(date),
+    short: new Intl.DateTimeFormat('hu-HU', {
+        year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric',
+    }).format(date)
+};
+};
+
 export default customDateFormats;
